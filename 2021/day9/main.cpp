23950c9ff23a34f9b9bca158e23d103c5ab0c070
Alex <alexbrochu1@gmail.com>
2021-12-12 00:33:58 -0500

    2021 - day 9 - Progress on part 1

--- 2021/day9/main.cpp
+++ 2021/day9/main.cpp
@@ -8,17 +8,35 @@
 #include <unordered_map>
 #include <vector>
 
-static std::string testPath = "./test_input.txt";
-static std::string path = "./input.txt";
+char getFieldValue(std::vector<std::vector<char>>& field, int y, int x)
+{
+    if (y < 0 || y > field.size()) return 0;
+    else if (x < 0 || x > field[0].size()) return 0;
+    else return field[y][x];
+}
+
+#define PATH "./test_input.txt"
+//#define PATH "./input.txt"
+
 int main(int argc, char** argv)
 {
-    auto file = std::ifstream(testPath);
-    //auto file = std::ifstream(path);
+    auto file = std::ifstream(PATH);
 
+    std::vector<std::vector<char>> field;
     std::string line;
+
+    int y = 0;
     while (std::getline(file, line))
     {
         printf("Line = %s\n", line.c_str());
+
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (i == 0) field.push_back(std::vector<char>(line.length()));
+            field[y][i] = line[i];
+        }
+
+        y++;
     }
 
     return 0;
