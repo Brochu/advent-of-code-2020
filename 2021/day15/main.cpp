40e09d4916077c080838ab8b207b817fa17e8fd7
Alex <alexbrochu1@gmail.com>
2021-12-18 14:52:52 -0500

    2021 - day 15 part 1 is done, need to add some wrappers to access data with modulo for next part

--- 2021/day15/main.cpp
+++ 2021/day15/main.cpp
@@ -1,12 +1,15 @@
 #include <array>
+#include <cmath>
+#include <cstdint>
 #include <fstream>
 #include <math.h>
 #include <queue>
 #include <string>
+#include <vector>
 
-#define PATH "./test_input.txt"
-//#define PATH "./input.txt"
-#define DIM 10
+//#define PATH "./test_input.txt"
+#define PATH "./input.txt"
+#define DIM 100
 
 typedef std::array<std::array<short, DIM>, DIM> Grid;
 typedef std::pair<int, int> Coord;
@@ -14,7 +17,7 @@ typedef std::pair<int, int> Coord;
 struct Node
 {
     Coord c;
-    int val;
+    uint64_t* val;
 };
 
 Grid parseGrid(std::ifstream& file)
@@ -34,12 +37,87 @@ Grid parseGrid(std::ifstream& file)
     return g;
 }
 
-int dijkstra(const Grid& grid, const Coord& start, const Coord& end)
+std::vector<Coord> genNeighbours(const Coord& c)
 {
-    //TODO: Create priority queue with custom comparer
-    std::array<std::array<int, DIM>, DIM> dist;
-    std::array<std::array<short*, DIM>, DIM> prev;
-    return 0;
+    std::vector<Coord> res;
+
+    if (c.first != DIM - 1)
+        res.push_back(std::make_pair(c.first + 1, c.second));
+
+    if (c.second != DIM - 1)
+        res.push_back(std::make_pair(c.first, c.second + 1));
+
+    return res;
+}
+
+uint64_t dijkstra(const Grid& grid, const Coord& start, const Coord& end)
+{
+    printf("[DIJKSTRA START]\n");
+
+    auto cmp = [](const Node& left, const Node& right){ return (*left.val ^ 1) > (*right.val ^ 1); };
+    std::priority_queue<Node, std::vector<Node>, decltype(cmp)> Q(cmp);
+
+    std::array<std::array<uint64_t, DIM>, DIM> dist;
+    for (int y = 0; y < DIM; y++)
+    {
+        for (int x = 0; x < DIM; x++)
+        {
+            dist[y][x] = INT32_MAX;
+
+            Node n { std::make_pair(x, y), &dist[y][x] };
+            Q.push(n);
+        }
+    }
+    dist[start.first][start.second] = 0;
+
+    //for (int i = 0; i < DIM*DIM; i++)
+    //{
+    //    auto n = Q.top();
+    //    Q.pop();
+
+    //    printf("(%i, %i), %i\n", n.c.first, n.c.second, *n.val);
+    //}
+
+    while(!Q.empty())
+    {
+        Node current = Q.top();
+        Q.pop();
+        //printf("(%i, %i) %ld\n", current.c.first, current.c.second, *current.val);
+
+        if (*current.val >= INT32_MAX) continue;
+
+        const int x = current.c.first;
+        const int y = current.c.second;
+
+        for (const Coord& c : genNeighbours(current.c))
+        {
+            const int nx = c.first;
+            const int ny = c.second;
+
+            uint64_t newDist = dist[y][x] + grid[ny][nx];
+            //printf("%ld + %i = %ld || %ld\n", dist[y][x], grid[ny][nx], newDist, dist[ny][nx]);
+            if (newDist < dist[ny][nx])
+            {
+                dist[ny][nx] = newDist;
+                Node n { std::make_pair(nx, ny), &dist[ny][nx] };
+                Q.push(n);
+            }
+        }
+    }
+    //auto test = Q.top();
+    //printf("(%i, %i) %ld\n", test.c.first, test.c.second, *test.val);
+
+    //for (int y = 0; y < DIM; y++)
+    //{
+    //    for (int x = 0; x < DIM; x++)
+    //    {
+    //        printf("%ld ", dist[y][x]);
+    //    }
+    //    printf("\n");
+    //}
+
+    printf("[DIJKSTRA END]\n");
+    return dist[end.second][end.first];
 }
 
 int main(int argc, char** argv)
@@ -56,8 +134,8 @@ int main(int argc, char** argv)
     //    printf("\n");
     //}
 
-    int risk = dijkstra(grid, std::make_pair(0, 0), std::make_pair(9, 9));
+    uint64_t risk = dijkstra(grid, std::make_pair(0, 0), std::make_pair(99, 99));
 
-    printf("\nResult = %i\n", risk);
+    printf("\nResult = %ld\n", risk);
     return 0;
 }
