1b437ae2d9a20906f51bc9d6bbeb0206e9638992
Alex <alexbrochu1@gmail.com>
2021-12-18 01:37:23 -0500

    2021 - day 14 - doesn't parse actual input properly

--- 2021/day14/main.cpp
+++ 2021/day14/main.cpp
@@ -1,44 +1,92 @@
 #include <algorithm>
 #include <fstream>
-#include <map>
 #include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
+
 
 //#define PATH "./test_input.txt"
 #define PATH "./input.txt"
 
-#define STEPS 20
+#define STEPS 10
 #define WINDOW 2
 
-std::map<size_t, char> expandStep(const std::string& start, std::map<std::string, char>& rules, std::vector<size_t>& counts)
+//TODO: Remember this, what is needed to use custom type as key for unordered containers
+struct Pair
 {
-    std::map<size_t, char> queuedInsert;
-    int offset = 0;
+    char l;
+    char r;
 
-    for (int i = 0; (i + WINDOW-1) < start.size(); i++)
+    bool operator==(const Pair& rhs) const
     {
-        std::string current;
-        for (int j = 0; j < WINDOW; j++)
-        {
-            current += start[i+j];
-        }
-        //printf("%s, %ld\n", current.c_str(), current.size());
+        return this->l == rhs.l &&  this->r == rhs.r;
+    }
+};
+template<> struct std::hash<Pair>
+{
+    std::size_t operator() (const Pair &s) const noexcept
+    {
+        std::size_t h1 = std::hash<char>{}(s.l);
+        std::size_t h2 = std::hash<char>{}(s.r);
 
-        auto ins = rules[current];
-        if (ins != 0)
-        {
-            queuedInsert[i+1+(offset++)] = ins;
-        }
+        return h1 ^ (h2 << 1);
+    }
+};
+typedef std::unordered_map<Pair, char> RuleMap;
+
+void debugPairFr(const std::unordered_map<Pair, size_t>& fr)
+{
+    for (auto [pair, count] : fr)
+    {
+        printf("[PFR] '%c%c' >> %ld\n", pair.l, pair.r, count);
+    }
+}
+void debugCharFr(const std::vector<uint64_t>& fr)
+{
+    for (int i = 0; i < 26; i++)
+    {
+        printf("[CFR] '%c' >> %ld\n", i + 'A', fr[i]);
+    }
+}
+void debugRules(const RuleMap& rules)
+{
+    for (auto [pair, c] : rules)
+    {
+        printf("[RULE] '%c%c' >> %c\n", pair.l, pair.r, c);
+    }
+}
+
+void step(const RuleMap& rules, std::unordered_map<Pair, uint64_t>& pairs, std::vector<uint64_t>& chars)
+{
+    printf("[STEPPING]\n");
+    auto newPairs = pairs;
+    auto newChars = chars;
+
+    for (auto [pair, count] : pairs)
+    {
+        if (rules.count(pair) == 0) continue;
+        char c = rules.at(pair);
+        //printf("Add '%c'\n", c);
+
+        newChars[c - 'A'] += count;
+        newPairs[pair] -= count;
+
+        newPairs[Pair{pair.l, c}] += count;
+        newPairs[Pair{c, pair.r}] += count;
     }
 
-    return queuedInsert;
+    chars = std::move(newChars);
+    pairs = std::move(newPairs);
 }
 
 int main(int argc, char** argv)
 {
     auto file = std::ifstream(PATH);
-    std::string start;
-    std::map<std::string, char> rules;
+    RuleMap rules;
+
+    std::vector<uint64_t> char_fr(26, 0);
+    std::unordered_map<Pair, uint64_t> pair_fr;
 
     std::string line;
     while (getline(file, line))
@@ -47,41 +95,42 @@ int main(int argc, char** argv)
 
         auto idx = line.find('-');
         if (idx != std::string::npos)
-            rules[line.substr(0, idx-1)] = line.substr(idx+3)[0];
+        {
+            std::string in = line.substr(0, idx-1);
+            rules.emplace(Pair{in[0], in[1]}, line.substr(idx+3)[0]);
+        }
         else
-            start = line;
+        {
+            std::string start = line;
+            for (int i = 0; i + WINDOW + 1<= start.size(); i++)
+            {
+                pair_fr.emplace(Pair{start[i], start[i+1]}, 1);
+                char_fr[start[i] - 'A']++;
+            }
+            char_fr[start[start.size()-1] - 'A']++;
+        }
     }
 
-    printf("[START][%ld] '%s'\n", start.length(), start.c_str());
-    //for (const std::pair<std::string, char>& r : rules)
-    //{
-    //    printf("[RULE] %s : %c\n", r.first.c_str(), r.second);
-    //}
-
-    std::vector<size_t> counts(26, 0);
-    for (const char& c : start)
-    {
-        if (c >= 'A')
-            counts[c - 'A']++;
-    }
+    debugPairFr(pair_fr);
+    printf("\n");
+    debugCharFr(char_fr);
+    printf("\n");
+    debugRules(rules);
 
     for (int i = 0; i < STEPS; i++)
     {
-        auto inserts = expandStep(start, rules, counts);
-        for (const std::pair<size_t, char>& ins : inserts)
-        {
-            start.insert(ins.first, 1, ins.second);
-            counts[ins.second - 'A']++;
-        }
-
-        printf("[step %i][%ld]\n", i, start.length());
+        step(rules, pair_fr, char_fr);
     }
+    debugCharFr(char_fr);
 
-    //auto end = std::remove_if(counts.begin(), counts.end(), [](const size_t& e){ return e <= 0; });
-    //auto minmax = std::minmax_element(counts.begin(), end);
-    //printf("min = %ld; max = %ld\n", *minmax.first, *minmax.second);
+    auto end = std::remove_if(char_fr.begin(), char_fr.end(), [](const uint64_t& e){ return e == 0; });
+    for (auto it = char_fr.begin(); it != end; it++)
+    {
+        printf("%ld\n", *it);
+    }
+    auto res = std::minmax_element(char_fr.begin(), end);
 
-    //size_t count = *minmax.second - *minmax.first;
-    //printf("\nResult = %ld\n", count);
+    printf("min = %ld, max = %ld\n", *res.first, *res.second);
+    printf("Result = %ld\n", *res.second - *res.first);
     return 0;
 }
