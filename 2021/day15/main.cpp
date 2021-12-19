7b49ddacacb32dda6655b27fb32287f658066978
Alex <alexbrochu1@gmail.com>
2021-12-19 14:23:09 -0500

    2021 - day 15 done

--- 2021/day15/main.cpp
+++ 2021/day15/main.cpp
@@ -5,9 +5,9 @@
 #include <set>
 #include <string>
 
-#define PATH "./test_input.txt"
-//#define PATH "./input.txt"
-#define DIM 10
+//#define PATH "./test_input.txt"
+#define PATH "./input.txt"
+#define DIM 100
 #define TILES 5
 
 typedef std::array<std::array<short, DIM>, DIM> Grid;
@@ -69,11 +69,18 @@ std::vector<Node> GenerateNeighbours(const Node& n)
     if (n.pos_y < (DIM * TILES) - 1)
         res.push_back(Node{ n.pos_x, n.pos_y + 1 });
 
+    if (n.pos_x > 0)
+        res.push_back(Node{ n.pos_x - 1, n.pos_y });
+
+    if (n.pos_y > 0)
+        res.push_back(Node{ n.pos_x, n.pos_y - 1 });
+
     return res;
 }
 
 uint64_t dijkstra(const Grid& g, uint start, uint end)
 {
+    //printf("(%i, %i) to (%i, %i)\n", start, start, end, end);
     auto cmp = [](const Node& l, const Node& r){ return l.dist > r.dist; };
     std::priority_queue<Node, std::vector<Node>, decltype(cmp)> Q(cmp);
     std::set<Coord> visited;
@@ -87,25 +94,61 @@ uint64_t dijkstra(const Grid& g, uint start, uint end)
         }
     }
     dist[start][start] = 0;
-    visited.insert(std::make_pair(start, start));
     Q.push(Node{ start, start, 0 });
+
+    while(!Q.empty())
+    {
+        auto q = Q.top();
+        Q.pop();
+
+        if (visited.find(q.getCoord()) == visited.end())
+        {
+            //printf("Checking (%i, %i) [%i] : ", q.pos_x, q.pos_y, fetchCoord(g, q.pos_y, q.pos_x));
+
+            visited.insert(q.getCoord());
+            for (Node n : GenerateNeighbours(q))
+            {
+                uint64_t newDist = dist[q.pos_y][q.pos_x] + fetchCoord(g, n.pos_y, n.pos_x);
+                //printf("(%i, %i) [%ld][%ld] ; ", n.pos_x, n.pos_y, newDist, dist[n.pos_y][n.pos_x]);
+
+                if (newDist < dist[n.pos_y][n.pos_x])
+                {
+                    dist[n.pos_y][n.pos_x] = newDist;
+
+                    n.dist = newDist;
+                    Q.push(n);
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    //for (int i = 0; i < DIM * TILES; i++)
+    //{
+    //    for (int j = 0; j < DIM * TILES; j++)
+    //    {
+    //        printf("%ld ", dist[i][j]);
+    //    }
+    //    printf("\n");
+    //}
+    printf("%ld\n", dist[end][end]);
 
     return 0;
 }
 
 int main(int argc, char** argv)
 {
-    auto file = std::ifstream(PATH);
+    std::ifstream file = std::ifstream(PATH);
     Grid grid = parseGrid(file);
 
-    for (int i = 0; i < DIM * TILES; i++)
-    {
-        for (int j = 0; j < DIM * TILES; j++)
-        {
-            printf("%i ", fetchCoord(grid, i, j));
-        }
-        printf("\n");
-    }
+    //for (int i = 0; i < DIM * TILES; i++)
+    //{
+    //    for (int j = 0; j < DIM * TILES; j++)
+    //    {
+    //        printf("%i ", fetchCoord(grid, i, j));
+    //    }
+    //    printf("\n");
+    //}
 
     uint64_t risk = dijkstra(grid, 0, (DIM * TILES) - 1);
     printf("\nResult = %ld\n", risk);
