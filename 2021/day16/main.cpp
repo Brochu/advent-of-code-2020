a02bc0e6050043eca71ff5d1b82cec141aefd032
Alex <alexbrochu1@gmail.com>
2021-12-20 09:38:38 -0500

    2021, day 16 progress, got litteral working

--- 2021/day16/main.cpp
+++ 2021/day16/main.cpp
@@ -27,6 +27,23 @@ static std::unordered_map<char, short> lutHex
     { 'F', 15 },
 };
 
+static std::unordered_map<int, int> lutMask
+{
+    { 0, 0 },
+    { 1, 1 },
+    { 2, 3 },
+    { 3, 7 },
+    { 4, 15 },
+};
+
+static std::unordered_map<int, int> lutMaskInd
+{
+    { 1, 8 },
+    { 2, 4 },
+    { 3, 2 },
+    { 4, 1 },
+};
+
 std::string parseFile(std::ifstream&& file)
 {
     std::string line;
@@ -50,7 +67,6 @@ std::bitset<5> fetchNextSegment(const std::string& raw, int& pos)
 {
     std::bitset<5> segment;
 
-
     int group = pos / 4;
     int idx = pos % 4;
     printf("[SEGMENT] group: %i, index: %i\n", group, idx);
@@ -62,12 +78,11 @@ std::bitset<5> fetchNextSegment(const std::string& raw, int& pos)
     printf("[SEGMENT] temp = %s\n", temp.to_string().c_str());
 
     int first = 4 - idx;
-    int mask = 0; //TODO: Build mask based off of first value, we want to catch the last 'first' bits in group byte
     int left = 5 - first;
     int shift = 4 - left;
     printf("[SEGMENT] first: %i, left: %i\n", first, left);
 
-    segment |= (lutHex[raw[group]]) & first;
+    segment |= (lutHex[raw[group]]) & lutMask[first];
     segment <<= left;
     segment |= (lutHex[raw[group+1]]) >> shift;
 
@@ -91,23 +106,69 @@ Header parseHeader(std::bitset<6> bin)
     return h;
 }
 
+struct Segment
+{
+    bool isEnd;
+    unsigned long value;
+};
+Segment parseSegment(std::bitset<5> bin)
+{
+    Segment s;
+
+    s.isEnd = !bin.test(4);
+    bin.set(4, 0);
+    s.value = bin.to_ulong();
+
+    return s;
+}
+
+unsigned long readLitteral(const std::string& packet, int& pos)
+{
+    unsigned long val = 0;
+    Segment s{};
+
+    while (!s.isEnd)
+    {
+        s = parseSegment(fetchNextSegment(packet, pos));
+        val <<= 4;
+        val |= s.value;
+    }
+
+    printf("Total = %ld\n", val);
+
+    return val;
+}
+
+void readOperation(const std::string& packet, int& pos)
+{
+    //TODO: Update this to read operations packet
+    printf("[OP] reading...");
+
+    int group = pos / 4;
+    int idx = pos % 4;
+    printf("[OP] group: %i, index: %i\n", group, idx);
+    bool type = (packet[group] & lutMaskInd[idx]) >> idx;
+    printf("[OP] %s\n", type ? "TOTAL LENGTH" : "SUB NUM");
+}
+
 int main(int argc, char** argv)
 {
     std::ifstream file = std::ifstream(PATH);
     int pos = 0;
-    std::string packet = parseFile(std::move(file));
+    //std::string packet = parseFile(std::move(file));
+    std::string packet = "38006F45291200";
 
     Header h = parseHeader(fetchHeader(packet, pos));
     printf("ver: %i; type: %i, next pos = %i\n", h.ver, h.type, pos);
 
-    auto test = fetchNextSegment(packet, pos);
-    printf("Got segment: %s\n", test.to_string().c_str());
-
-    auto test1 = fetchNextSegment(packet, pos);
-    printf("Got segment: %s\n", test1.to_string().c_str());
-
-    auto test2 = fetchNextSegment(packet, pos);
-    printf("Got segment: %s\n", test2.to_string().c_str());
+    if (h.type == 4) // Litteral
+    {
+        unsigned long value = readLitteral(packet, pos);
+    }
+    else
+    {
+        readOperation(packet, pos);
+    }
 
     printf("\nResult = %ld\n", 0L);
     return 0;
